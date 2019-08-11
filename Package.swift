7df45f220d38041be9d40e73877d2f8d5633e917
Rene Hexel <r.hexel@griffith.edu.au>
2019-08-12 09:23:32 +1000

    s/libgtk3.0-dev/libgtk-3-dev/ and remove libgdk3.0-dev

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(name: "CGtk",
 	pkgConfig: "gtk-3.0 gdk-3.0 pangocairo pangoft2 pango gio-unix-2.0 glib-2.0",
 	providers: [
 		.brew(["gtk+3", "glib", "glib-networking", "gobject-introspection"]),
-		.apt(["libgtk3.0-dev", "libgdk3.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+		.apt(["libgtk-3-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
 	]
 )
